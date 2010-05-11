e5f430bd635f60036684aad473f72f32c5e56938
Nicos Gollan <ngollan@h1674689.stratoserver.net>
2010-05-11 14:54:43 +0200

    Added license, explanatory comment, attribution

--- jsongate.py
+++ jsongate.py
@@ -1,14 +1,28 @@
 #!/usr/bin/env python
+#
+# Short demo for the Ice-to-JSON callback gateway. It will just sit there
+# and try to send authentication and information requests to an HTTP server
+# on localhost:10000, which likely means you get an error message every time
+# a user logs in to a server.
+#
+# To set the Slice file location, make sure the environment variable
+# MURMUR_SLICE_PATH is set correctly (default: '/usr/share/slice/Murmur.ice'
+# which should work on Debian-based Linux distributions).
+#
+# This code is based on Chris Heald's implementation of 'comet-server',
+# available as part of murmur-manager:
+#            http://github.com/cheald/murmur-manager
 
 import Ice, sys, os
 
 from murmur import Murmur
 from murmur.json.authenticator import ServerAuthenticatorI
 
-httpconn = False
-
 def add_authenticator(server, adapter):
-	server_authenticator = Murmur.ServerAuthenticatorPrx.uncheckedCast(adapter.addWithUUID(ServerAuthenticatorI(server, adapter)))
+	authenticator = ServerAuthenticatorI(server, adapter)
+	authenticator.base_uri = 'http://localhost:10000/'
+
+	server_authenticator = Murmur.ServerAuthenticatorPrx.uncheckedCast(adapter.addWithUUID(authenticator))
 	server.setAuthenticator(server_authenticator)
 
 class MetaCallbackI(Murmur.MetaCallback):
